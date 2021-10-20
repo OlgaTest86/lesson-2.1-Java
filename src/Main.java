025278ab82dd0046ad0a760351baf898b6e76940
Dudkina Olga <dudkinaolia@yandex.ru>
2021-10-20 19:22:40 +0300

    Miles

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCoast = 20000;
+        int oneMile = 20;
+        int totalMile = ticketCoast / oneMile;
+
+        System.out.println(totalMile);
+    }
+}
